16b3ce62bb7f4916f6da058e93d973234e18bdc0
Arpita Sahoo <arpitasahoo795@gmail.com>
2025-08-29 23:42:29 +0530

    Add files via upload

---
+++ UsingThreadClass/B.java
@@ -0,0 +1,11 @@
+package Multithreading.UsingThreadClass;
+
+public class B extends Thread{
+    public void run(){
+        for (int i=0;i<100;i++){
+            System.out.println("Hello");
+        }
+    }
+}
+
+
